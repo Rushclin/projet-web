6bc2d4463807078a0fb9ddca107f84046ac0b555
Rushclin <68521991+Rushclin@users.noreply.github.com>
2022-05-12 15:51:09 +0200

    Delete Documents/WorkSpaces/UV Projet directory

--- Documents/WorkSpaces/UV Projet/webpharmacy/src/_mock/account.js
+++
@@ -1,9 +0,0 @@
-// ----------------------------------------------------------------------
-
-const account = {
-  displayName: "Takam Rushclin",
-  email: "takamrushclin@gmail.com",
-  photoURL: "/static/assets/images/PhotoGrid_Site_1647717549345.jpg",
-};
-
-export default account;
